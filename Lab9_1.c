399868fcf422a1fe75bc17411667570d2b956a86
HWHung <howard830111@yahoo.com.tw>
2014-12-16 19:21:34 +0800

    Update Lab9_1.c

--- Lab9_1.c
+++ Lab9_1.c
@@ -1,15 +1,12 @@
 #include<REG_MPC82G516.H>
 
-//sbit P2_1 = P2 ^ 1;
-//sbit P2_0 = P2 ^ 0;
-
 void delay_ms(int t){
 	int i;
 	while(t--)
 		for(i=0;i<1000;++i);
 }
 
-unsigned char read_min, read_max;
+unsigned char read_min;
 
 void main()
 {
@@ -18,26 +15,13 @@ void main()
 		P21 = 0;
 		P21 = 1;
 		while(P20 == 1);
+		//P0 = P1;
+		//P0 = (read_min/16)<<4 + (read_min%16);
+		
 		read_min = P1;
-		read_min -= 0;	//can change
-		read_max = 1;	//can change
-		read_min /= read_max;
+		read_min -= 16;	//can change
+		read_min /= 32;//can change
 		P0 = read_min;
 		delay_ms(150);
 	}
-}/*
-void main()
-{
-	while(1)
-	{
-		P2^1 = 0;
-		P2^1 = 1;
-		while(P2^0 == 1);
-		A = P1;
-		A = A - C - 0;	//can change
-		B = 1;	//can change
-		A /= B;
-		P0 = A;
-		delay_ms(150);
-	}
-}*/
+}
